dc8a74978e3ad7d0acc8b3189f9984f5b09c0370
rbernal1992 <54275705+rbernal1992@users.noreply.github.com>
2019-10-14 18:41:32 -0500

    added previous function

--- Lab_6/js/indexd.js
+++ Lab_6/js/indexd.js
@@ -24,18 +24,24 @@ function watchForm(){
 			success: function(responseJson){
 				console.log(responseJson.items);
 				globaljson = responseJson;
+				$('#items').html('');
 				 //for(i in responseJson.items){ for each in javascript
-					for(var i = counterposition; i < counterposition + 5; i++){
+					for(var i = counterposition; i < counterposition + 10; i++){
 					 let thumbnail = responseJson.items[i].snippet.thumbnails.medium.url;
 					 let titlevideo = responseJson.items[i].snippet.title;
 					//console.log(responseJson.items[i].id.videoId);
+					
 					$('#items').append(`<li> <div width="60%" align="center"> <h3>`+ titlevideo +`</h3> <a href="https://www.youtube.com/watch?v=` + responseJson.items[i].id.videoId + `" target="_blank"> <img src="`+thumbnail+`" width = "30%" height="30%"></a>
 					</div></li>  
 					`)
 					
 				}
 				$('#addmore').show();
-				counterposition = counterposition + 5;
+				$('#previous').show();
+				if(counterposition == 50)
+				counterposition = 0;
+				else
+				counterposition = counterposition + 10;
 			},
 			error: function(err){
 				console.log("Error algo salio mal");
@@ -51,17 +57,42 @@ function watchForm(){
 
 	 	$('#addmore').on('click',function(event){
 			event.preventDefault();
-				for(var i = counterposition; i < counterposition + 5; i++){
+			$('#items').html('');
+				for(var i = counterposition; i < counterposition + 10; i++){
 					 let thumbnail = globaljson.items[i].snippet.thumbnails.medium.url;
 					 let titlevideo = globaljson.items[i].snippet.title;
 					//console.log(responseJson.items[i].id.videoId);
+					
 					$('#items').append(`<li> <div width="60%" align="center"> <h3>`+ titlevideo +`</h3> <a href="https://www.youtube.com/watch?v=` + globaljson.items[i].id.videoId + `" target="_blank"> <img src="`+thumbnail+`" width = "30%" height="30%"></a>
 					</div></li>  
 					`)
 					
 				}
-				if(counterposition < globaljson.items.length - 5)
-				counterposition = 5 + counterposition;
+				if(counterposition == 50)
+				counterposition = 0;
+				else
+				counterposition = counterposition + 10;
+				
+		});
+		
+		$('#previous').on('click',function(event){
+			event.preventDefault();
+			$('#items').html('');
+			if(counterposition > 10)
+			{
+				counterposition = counterposition - 20;
+			}
+				for(var i = counterposition; i < counterposition + 10; i++){
+					 let thumbnail = globaljson.items[i].snippet.thumbnails.medium.url;
+					 let titlevideo = globaljson.items[i].snippet.title;
+					//console.log(responseJson.items[i].id.videoId);
+					
+					$('#items').append(`<li> <div width="60%" align="center"> <h3>`+ titlevideo +`</h3> <a href="https://www.youtube.com/watch?v=` + globaljson.items[i].id.videoId + `" target="_blank"> <img src="`+thumbnail+`" width = "30%" height="30%"></a>
+					</div></li>  
+					`)
+					
+				}
+				
 				
 		});
 
